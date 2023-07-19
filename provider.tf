11840cca74d850b0c0858f8bcd3e43797deb703b
Ubuntu <ubuntu@ip-172-31-94-192.ec2.internal>
2023-07-19 10:04:10 +0000

    Adding provider and repo files

---
+++ provider.tf
@@ -0,0 +1,6 @@
+# Configure the GitHub Provider
+provider "github" {
+  version      = "~> 2.2"
+  organization = "https://github.com/ShekharchandraR"
+  # Export the GITHUB_TOKEN as an environment variable
+}
---
+++ repository.tf
@@ -0,0 +1,14 @@
+resource "github_repository" "github-management" {
+  name        = "github-management"
+  description = "Terraform based repository to manage all our GutHub repositories"
+
+  private            = false
+  has_issues         = true
+  has_wiki           = false
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  auto_init          = false
+  license_template   = "mit"
+  topics             = ["config", "terraform"]
+}
